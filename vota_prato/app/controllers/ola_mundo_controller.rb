08d44055a3ac3274f5a2024dd8fcd1e5fd0ebe35
Yuri Faro <yurifarod@yahoo.com.br>
2018-04-03 17:29:47 -0300

    Mais andamento no projeto

---
+++ vota_prato/app/controllers/ola_mundo_controller.rb
@@ -0,0 +1,5 @@
+class OlaMundoController < ApplicationController
+  def index
+    @greeting = "Home action says: Hello world!"
+  end
+end
--- vota_prato/config/routes.rb
+++ vota_prato/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  match 'ola' => 'ola_mundo#index', via: 'get'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+VotaPrato::Application.routes.draw do
+  match 'ola' => 'ola_mundo#index', via: 'get'
 end
