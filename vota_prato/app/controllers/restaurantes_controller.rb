43d2de7c0442e950dc56f2f0c577a42361a7637d
yurifarod <yurifarod@yahoo.com.br>
2018-04-20 20:44:36 -0300

    add mais aulas

--- vota_prato/app/controllers/restaurantes_controller.rb
+++ vota_prato/app/controllers/restaurantes_controller.rb
@@ -2,4 +2,23 @@ class RestaurantesController < ApplicationController
   def index
     @restaurantes = Restaurante.order :nome
   end
+
+  def show
+    @restaurantes = Restaurante.find(params[:id])
+  end
+
+  def new
+    @restaurantes = Restaurante.new
+  end
+
+  def destroy
+    @restaurantes = Restaurante.find(params[:id])
+    @restaurantes.destroy
+    redirect_to(action: "index")
+  end
+
+  def create
+    @restaurante = Restaurante.new params.require(:restaurante).permit(:nome, :endereco, :especialidade)
+    @restaurante.save
+  end
 end
--- vota_prato/config/routes.rb
+++ vota_prato/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  match 'ola' => 'ola_mundo#index', via: 'get'
-  match 'restaurantes' => 'restaurantes#index', via: 'get'
-  #resources: restaurantes
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-VotaPrato::Application.routes.draw do
+  resources :comentarios
+  resources :clientes
+  resources :qualificacoes
+  resources :clientes
+  resources :restaurantes
   match 'ola' => 'ola_mundo#index', via: 'get'
 end
