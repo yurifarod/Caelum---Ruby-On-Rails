ed9126efc808690f04b30ac78d60a51d4e09b515
yurifarod <yurifarod@yahoo.com.br>
2018-06-13 23:49:27 -0300

    Quase acabando!

--- vota_prato/app/models/cliente.rb
+++ vota_prato/app/models/cliente.rb
@@ -1,2 +1,13 @@
-class Cliente < ApplicationRecord
+class Cliente < ActiveRecord::Base
+
+has_many :qualificacao
+
+validates_presence_of :nome, message: " - deve ser preenchido"
+
+validates_uniqueness_of :nome, message: " - nome já cadastrado"
+
+validates_numericality_of :idade, greater_than: 0,
+                                  less_than: 100,
+                                  message: " - deve ser um número entre 0 e 100"
+
 end
